8a407582f10b3aba84ab4d2051c88fa5f8bb47d9
Limchangsu <skyisthelimitless@gmail.com>
2021-02-23 12:38:16 +0900

    first commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const helloWorld = () => console.log('Hello World!');
+import { ChainId, Token } from "@uniswap/sdk";
 
-helloWorld();
+const chainId = ChainId.MAINNET;
+const tokenAddress = "0x6B175474E89094C44Da98b954EedeAC495271d0F"; // must be checksummed
+const decimals = 18;
+
+const DAI = new Token(chainId, tokenAddress, decimals);
+
+console.log(DAI);
